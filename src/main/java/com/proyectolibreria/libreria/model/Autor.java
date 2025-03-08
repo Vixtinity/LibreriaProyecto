1541f3a4e1540d69b6d5abc0ec7706d9d7fe607e
Ismael <08062006ismael@gmail.com>
2025-03-08 20:01:38 +0100

    Fix base de datos

--- src/main/java/com/proyectolibreria/libreria/model/Autor.java
+++ src/main/java/com/proyectolibreria/libreria/model/Autor.java
@@ -1,5 +1,51 @@
 package com.proyectolibreria.libreria.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Autor {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String nacionalidad;
+    private String fechaNacimiento;
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
