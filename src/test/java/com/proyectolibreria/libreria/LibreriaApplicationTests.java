d38762e53080550a831295e8785f3fb226063617
Ismael <08062006ismael@gmail.com>
2025-03-08 16:18:12 +0100

    primer commit

---
+++ src/test/java/com/proyectolibreria/libreria/LibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectolibreria.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
