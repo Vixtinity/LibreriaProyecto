5b4165076e503e5c546be769dc5fec52ef56550a
Ismael <08062006ismael@gmail.com>
2025-03-09 16:33:17 +0100

    Usuario controller

--- src/main/java/com/proyectolibreria/libreria/controller/UsuarioController.java
+++ src/main/java/com/proyectolibreria/libreria/controller/UsuarioController.java
@@ -1,5 +1,27 @@
 package com.proyectolibreria.libreria.controller;
 
+import com.proyectolibreria.libreria.model.Usuario;
+import com.proyectolibreria.libreria.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-    
+    @Autowired
+    private UsuarioService usuarioService;
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioService.listarUsuarios();
+    }
+    // Obtener un usuario por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.obtenerUsuarioPorId(id);
+        return usuario.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
